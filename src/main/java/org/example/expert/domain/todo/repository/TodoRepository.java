02672a0b63059a6d6bd1873bb9201044f4fe1ec7
pookyspooky <union0802@naver.com>
2024-10-08 23:33:42 +0900

    [fix] TodoRepository Optimize Imports

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,15 +1,13 @@
 package org.example.expert.domain.todo.repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import org.example.expert.domain.todo.entity.Todo;
-import org.hibernate.query.criteria.JpaExpression;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
